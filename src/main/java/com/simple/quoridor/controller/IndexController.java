dbd24de170a5a21d810b538576ce942b7e6c778e
leesangpil <leesangpil@leesangpilui-MacBook-Pro.local>
2017-02-10 01:51:22 +0900

    some files rename and create pakages

---
+++ src/main/java/com/simple/quoridor/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.simple.quoridor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("/index");
+        mav.addObject("projectName", "쿼리도");
+
+        return mav;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>쿼리도</title>
+</head>
+<body>
+<h1>
+    ${projectName}의 세계에 오신것을 환영합니다!
+</h1>
+</body>
+</html>
