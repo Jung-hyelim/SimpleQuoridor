b3256eaca5f0ca223023a0e409b241829e46a311
leesangpil <leesangpil@leesangpilui-MacBook-Pro.local>
2017-02-14 23:26:49 +0900

    Add lombok

--- src/main/java/com/simple/quoridor/controller/IndexController.java
+++ src/main/java/com/simple/quoridor/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.simple.quoridor.controller;
 
+import com.simple.quoridor.model.ExampleModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +14,9 @@ public class IndexController {
         ModelAndView mav = new ModelAndView("/index");
         mav.addObject("projectName", "쿼리도");
         mav.addObject("projectMember", "정혜림 & 이상필");
-
+        // for lombok test
+        ExampleModel model = new ExampleModel();
+        model.setId("jung hye lim");
         return mav;
     }
 
--- src/main/java/com/simple/quoridor/model/ExampleModel.java
+++ src/main/java/com/simple/quoridor/model/ExampleModel.java
@@ -1,7 +1,11 @@
 package com.simple.quoridor.model;
 
+import lombok.Data;
+
 /**
  * Created by leesangpil on 2017. 2. 14..
  */
+@Data
 public class ExampleModel {
+    private String id;
 }
